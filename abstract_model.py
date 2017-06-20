e39c96d77e95a2eb3965233c6dfbb6392aad97cd
Nikola Valesova <bihanojko@localhost.localdomain>
2017-06-20 16:06:22 +0200

    Some comments added, minor changes

--- abstract_model.py
+++ abstract_model.py
@@ -20,9 +20,12 @@ def json_to_specfile_class(json_containing_parsed_spec):
     for single_block in json_containing_parsed_spec:
         single_block['next'] = None
 
+        # Header Tag
         if single_block['block_type'] == BlockTypes.HeaderTagType:
             Specfile.headerTags.append(remove_blocktype(single_block))
             next_field = Specfile.headerTags[-1]
+
+        # Section Tag
         elif single_block['block_type'] == BlockTypes.SectionTagType:
             if 'package' not in single_block['keyword']:
                 Specfile.sectionTags.append(remove_blocktype(single_block))
@@ -40,18 +43,28 @@ def json_to_specfile_class(json_containing_parsed_spec):
                 Specfile.conditions.append(remove_blocktype(single_block))
                 next_field = Specfile.sectionTags[-1]
                 previous_node_next_pointer = point_package_to_ptr
+
+        # Macro Definition
         elif single_block['block_type'] == BlockTypes.MacroDefinitionType:
             Specfile.macroDefinitions.append(remove_blocktype(single_block))
             next_field = Specfile.macroDefinitions[-1]
+
+        # Macro Condition
         elif single_block['block_type'] == BlockTypes.MacroConditionType:
             Specfile.macroConditions.append(remove_blocktype(single_block))
             next_field = Specfile.macroConditions[-1]
+        
+        # Macro Undefinition
         elif single_block['block_type'] == BlockTypes.MacroUndefinitionType:
             Specfile.macroUndefinitions.append(remove_blocktype(single_block))
             next_field = Specfile.macroUndefinitions[-1]
+
+        # Commentary
         elif single_block['block_type'] == BlockTypes.CommentType:
             Specfile.comments.append(remove_blocktype(single_block))
             next_field = Specfile.comments[-1]
+        
+        # Condition
         elif single_block['block_type'] == BlockTypes.ConditionType:
             content = single_block['content']
             else_body = single_block['else_body']
@@ -68,7 +81,6 @@ def json_to_specfile_class(json_containing_parsed_spec):
                 single_block['else_body'] = tmp['next']                
             Specfile.conditions.append(remove_blocktype(single_block))
             next_field = Specfile.conditions[-1]
-            # previous_node_next_pointer.update({'next': point_condition_to_ptr})
             previous_node_next_pointer = point_condition_to_ptr
 
         previous_node_next_pointer.update({'next': next_field})
@@ -85,6 +97,3 @@ previous_node_next_pointer = Specfile.beginning
 json_to_specfile_class(json_containing_parsed_spec['block_list'])
 
 print(json.dumps(Specfile, default=lambda o: o.__dict__, sort_keys=True))
-
-# print(ctypes.cast(previous_node_next_pointer, ctypes.py_object))
-# pprint(json_containing_parsed_spec)
